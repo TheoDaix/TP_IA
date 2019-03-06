3b78eb8c00c0aea48880c59a26de38de2ae96384
allan <allandu1@live.fr>
2019-03-06 15:54:36 +0100

    ucs et A* (coop salon bleu)

--- search/search.py
+++ search/search.py
@@ -20,147 +20,172 @@ Pacman agents (in searchAgents.py).
 import util
 
 class Node:
-    def __init__(self, parentAction, parent, problem,state):
-        self.parentAction = parentAction
-        self.parent = parent
-        self.problem = problem
-        self.state = state
-        
-    def __eq__(self,other):
-        if not isinstance(other, Node):
-            return False
-        return self.parentAction == other.parentAction and self.parent == other.parent and self.problem== other.problem and self.problem==other.problem 
+	def __init__(self, parentAction, parent, problem,state, cost = 0):
+		self.parentAction = parentAction
+		self.parent = parent
+		self.problem = problem
+		self.state = state
+		self.cost = cost
+		
+	def __eq__(self,other): 
+		if not isinstance(other, Node):
+			return False
+		return self.state==other.state 
 
 class SearchProblem:    
-    """
-    This class outlines the structure of a search problem, but doesn't implement
-    any of the methods (in object-oriented terminology: an abstract class).
+	"""
+	This class outlines the structure of a search problem, but doesn't implement
+	any of the methods (in object-oriented terminology: an abstract class).
 
-    You do not need to change anything in this class, ever.
-    """
+	You do not need to change anything in this class, ever.
+	"""
 
-    def getStartState(self):
-        """
-        Returns the start state for the search problem.
-        """
-        util.raiseNotDefined()
+	def getStartState(self):
+		"""
+		Returns the start state for the search problem.
+		"""
+		util.raiseNotDefined()
 
-    def isGoalState(self, state):
-        """
-          state: Search state
+	def isGoalState(self, state):
+		"""
+		  state: Search state
 
-        Returns True if and only if the state is a valid goal state.
-        """
-        util.raiseNotDefined()
+		Returns True if and only if the state is a valid goal state.
+		"""
+		util.raiseNotDefined()
 
-    def getSuccessors(self, state):
-        """
-          state: Search state
+	def getSuccessors(self, state):
+		"""
+		  state: Search state
 
-        For a given state, this should return a list of triples, (successor,
-        action, stepCost), where 'successor' is a successor to the current
-        state, 'action' is the action required to get there, and 'stepCost' is
-        the incremental cost of expanding to that successor.
-        """nnnnnnn
+		For a given state, this should return a list of triples, (successor,
+		action, stepCost), where 'successor' is a successor to the current
+		state, 'action' is the action required to get there, and 'stepCost' is
+		the incremental cost of expanding to that successor.
+		"""
 
-        util.raiseNotDefined()
+		util.raiseNotDefined()
 
-    def getCostOfActions(self, actions):
-        """
-         actions: A list of actions to take
+	def getCostOfActions(self, actions):
+		"""
+		 actions: A list of actions to take
 
-        This method returns the total cost of a particular sequence of actions.
-        The sequence must be composed of legal moves.
-        """
-        util.raiseNotDefined()
+		This method returns the total cost of a particular sequence of actions.
+		The sequence must be composed of legal moves.
+		"""
+		util.raiseNotDefined()
 
 
 def tinyMazeSearch(problem):
-    """
-    Returns a sequence of moves that solves tinyMaze.  For any other maze, the
-    sequence of moves will be incorrect, so only use this for tinyMaze.
-    """
-    from game import Directions
-    s = Directions.SOUTH
-    w = Directions.WEST
-    return  [s, s, w, s, w, w, s, w]
+	"""
+	Returns a sequence of moves that solves tinyMaze.  For any other maze, the
+	sequence of moves will be incorrect, so only use this for tinyMaze.
+	"""
+	from game import Directions
+	s = Directions.SOUTH
+	w = Directions.WEST
+	return  [s, s, w, s, w, w, s, w]
 
 def depthFirstSearch(problem):
-    """
-    Search the deepest nodes in the search tree first.
+	"""
+	Search the deepest nodes in the search tree first.
 
-    Your search algorithm needs to return a list of actions that reaches the
-    goal. Make sure to implement a graph search algorithm.
+	Your search algorithm needs to return a list of actions that reaches the
+	goal. Make sure to implement a graph search algorithm.
 
-    To get started, you might want to try some of these simple commands to
-    understand the search problem that is being passed in:
+	To get started, you might want to try some of these simple commands to
+	understand the search problem that is being passed in:
 
-    print "Start:", problem.getStartState()
-    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
-    print "Start's successors:", problem.getSuccessors(problem.getStartState())
-    """
-    
-    start = Node(None,None,problem,problem.getStartState())
-    return recursiveDfs(start, problem,[problem.getStartState()])[1:]
-            
+	print "Start:", problem.getStartState()
+	print "Is the start a goal?", problem.isGoalState(problem.getStartState())
+	print "Start's successors:", problem.getSuccessors(problem.getStartState())
+	"""
+	
+	start = Node(None,None,problem,problem.getStartState())
+	return recursiveDfs(start, problem,[problem.getStartState()])[1:]
+			
 def recursiveDfs(node, problem,exploredSet):
-    if (problem.isGoalState(node.state)):
-        return [node.parentAction]
-    else:
-        for child in problem.getSuccessors(node.state):
-            (childState, action, cost) = child
-            if not(childState in exploredSet):
-                exploredSet.append(childState)  
-                childNode = Node(action, node, problem, childState)
-                result = recursiveDfs(childNode, problem,exploredSet) 
-                if result != None:
-                    return [node.parentAction] + result
+	if (problem.isGoalState(node.state)):
+		return [node.parentAction]
+	else:
+		for child in problem.getSuccessors(node.state):
+			(childState, action, cost) = child
+			if not(childState in exploredSet):
+				exploredSet.append(childState)  
+				childNode = Node(action, node, problem, childState)
+				result = recursiveDfs(childNode, problem,exploredSet) 
+				if result != None:
+					return [node.parentAction] + result
 
 def breadthFirstSearch(problem):
-    """Search the shallowest nodes in the search tree first."""
-    start = Node(None,None,problem,problem.getStartState())
-    frontier = util.Queue()
-    frontier.push(start)
-    exploredSet = []
-    while not(frontier.isEmpty()):
-        node = frontier.pop()
-        if (problem.isGoalState(node.state)):
-            return solution(node)[1:]
-        else:
-            exploredSet.append(node.state)
-            for child in problem.getSuccessors(node.state): 
-                (childState, action, cost) = child
-                childNode = Node(action, node, problem, childState)
-                if  not(childState in exploredSet):
-                    inFrontier = False
-                    for front in frontier.list:
-                        if front.state == childState:
-                            inFrontier = True     #gérer childNode in frontier
-                    if not(inFrontier):
-                        frontier.push(childNode)
-                    
+	"""Search the shallowest nodes in the search tree first."""
+	start = Node(None,None,problem,problem.getStartState())
+	frontier = util.Queue()
+	frontier.push(start)
+	exploredSet = []
+	while not(frontier.isEmpty()):
+		node = frontier.pop()
+		if (problem.isGoalState(node.state)):
+			return solution(node)[1:]
+		else:
+			exploredSet.append(node.state)
+			for child in problem.getSuccessors(node.state): 
+				(childState, action, cost) = child
+				childNode = Node(action, node, problem, childState)
+				if not(childState in exploredSet or childNode in frontier.list): 
+					frontier.push(childNode)
+					
 def solution(node):
-    if node == None:
-        return []
-    else:
-        return solution(node.parent)+ [node.parentAction]
-        
+	if node == None:
+		return []
+	else:
+		return solution(node.parent)+ [node.parentAction]
+		
 def uniformCostSearch(problem):
-    """Search the node of least total cost first."""
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+	"""Search the node of least total cost first."""
+	start = Node(None,None,problem,problem.getStartState())
+	frontier = util.PriorityQueue()
+	frontier.push(start,start.cost)
+	exploredSet = []
+	while not(frontier.isEmpty()):
+		node = frontier.pop()
+		if (problem.isGoalState(node.state)):
+			return solution(node)[1:]
+		else:
+			exploredSet.append(node.state)
+			for child in problem.getSuccessors(node.state): 
+				(childState, action, cost) = child
+				childNode = Node(action, node, problem, childState, node.cost + cost)
+				if not(childState in exploredSet):
+					frontier.update(childNode,childNode.cost)
+				
 
 def nullHeuristic(state, problem=None):
-    """
-    A heuristic function estimates the cost from the current state to the nearest
-    goal in the provided SearchProblem.  This heuristic is trivial.
-    """
-    return 0
+	"""
+	A heuristic function estimates the cost from the current state to the nearest
+	goal in the provided SearchProblem.  This heuristic is trivial.
+	"""
+	return 0
 
 def aStarSearch(problem, heuristic=nullHeuristic):
-    """Search the node that has the lowest combined cost and heuristic first."""
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+	"""Search the node that has the lowest combined cost and heuristic first."""
+	def heuristicCost(node):
+		return node.cost + heuristic(node.state,problem)
+	start = Node(None,None,problem,problem.getStartState())
+	frontier = util.PriorityQueue()
+	frontier.push(start,heuristicCost(start))
+	exploredSet = []
+	while not(frontier.isEmpty()):
+		node = frontier.pop()
+		if (problem.isGoalState(node.state)):
+			return solution(node)[1:]
+		else:
+			exploredSet.append(node.state)
+			for child in problem.getSuccessors(node.state): 
+				(childState, action, cost) = child
+				childNode = Node(action, node, problem, childState, node.cost + cost)
+				if not(childState in exploredSet):
+					frontier.update(childNode,heuristicCost(childNode))
 
 
 # Abbreviations
